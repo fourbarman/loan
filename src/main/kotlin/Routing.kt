868f3919d8769896b1c2def5cf02436fb7daa90f
fourbarman <maks.java@yandex.ru>
2026-03-21 12:28:44 +0400

    delete unused code

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,17 +3,9 @@ package ru.job4j
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import ru.job4j.controller.ContractController
 
-//fun Application.configureRouting() {
-//    routing {
-//        get("/") {
-//            call.respondText("Hello World!")
-//        }
-//    }
-//}
 fun Application.configureRouting(
     contractController: ContractController
 ) {
